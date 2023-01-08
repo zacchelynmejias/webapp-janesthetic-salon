e6bdc5383891fab18aa293700c5cd2c18975af7d
zacchelynmejias <zacchelynmejias@gmail.com>
2023-01-08 20:18:50 +0800

    changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -520,11 +520,7 @@
 
           <div class="col-lg-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
             <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>JC AQUINO st. 2ndfloor of CRISPY KING across OCTOGAN computer store, near DESMARK, Butuan City, Philippines</p>
-              </div>
+               
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
@@ -538,7 +534,13 @@
                 <p>0946 073 0701</p>
               </div>
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>JC AQUINO st. 2ndfloor of CRISPY KING across OCTOGAN computer store, near DESMARK, Butuan City, Philippines</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15765.23859920649!2d125.51273703575136!3d8.943587436969107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3301c19d8a0cde69%3A0xdc16c0a63d6ed07b!2sJanesthetic%20Salon!5e0!3m2!1sen!2sph!4v1671597975233!5m2!1sen!2sph" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
             </div>
 
           </div>
