a0b60d6cc09b29c47c3e20d1e15453547f1f7c59
zacchelynmejias <zacchelynmejias@gmail.com>
2023-01-08 19:20:39 +0800

    embedded map

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -546,29 +546,14 @@
           <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Your Name</label>
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="form-group col-md-6 mt-3 mt-md-0">
-                  <label for="name">Your Email</label>
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
+                
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15765.23859920649!2d125.51273703575136!3d8.943587436969107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3301c19d8a0cde69%3A0xdc16c0a63d6ed07b!2sJanesthetic%20Salon!5e0!3m2!1sen!2sph!4v1671597975233!5m2!1sen!2sph" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
+              
               </div>
-              <div class="form-group mt-3">
-                <label for="name">Subject</label>
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="10" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+               
+
+               
+               
             </form>
           </div>
 
